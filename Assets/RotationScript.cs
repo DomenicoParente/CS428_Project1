1194b14862c8849e48f4f1151a0f96dcc9732f5c
DomenicoParente <domenico.parente@mail.polimi.it>
2021-09-17 16:14:49 -0500

    added animation

---
+++ Assets/RotationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    //Assign a GameObject in the Inspector to rotate around
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, target.transform.up , 50 * Time.deltaTime);
+    }
+}
